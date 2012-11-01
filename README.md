4ad6165931acdd7cca3e0266080e9a64019e4b6f
Chris LeBlanc <chris@eedfw.us>
2012-11-01 12:23:37 -0500

    Updating documetation. Fixing ip_address param.

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-eedfw_geoplugin
+EEdfw Geoplugin
 ===============
 
 Module to return a user's geolocation information via the GeoPlugin service (http://www.geoplugin.com/).
+
+Usage
+-----
+
+### {exp:eedfw_geoplugin:locate}
+
+Locates a user.
+
+#### Parameters
+
+ip_address
+Specify a users email address. Default: Current user's ip.
+
+#### Variables
+
+{ip}
+{city}
+{region}
+{area_code}
+{dma_code}
+{country_name}
+{continent_code}
+{latitute}
+{longitude}
+{currency_code}
+{currency_symbol}
+{currency_converter}
--- mod.eedfw_geoplugin.php
+++ mod.eedfw_geoplugin.php
@@ -4,20 +4,35 @@ require_once('includes/geoplugin.class.php');
 class Eedfw_geoplugin
 {
 	private $settings;
-	
-	public function __construct() {
+
+	public function __construct()
+	{
 		$this->EE =& get_instance();
 		$this->EE->load->model('Eedfw_geoplugin_settings_model');
 
 		$this->settings = $this->EE->Eedfw_geoplugin_settings_model->get();
 		$this->geoplugin = new geoPlugin();
-		
-		$this->ip_address = $this->EE->TMPL->fetch_param('ip_address', $this->EE->input->ip_address());
 	}
 
-	public function locate() {
+	public function locate()
+	{
+		$this->ip_address = $this->EE->TMPL->fetch_param('ip_address', $this->EE->input->ip_address());
+
 		$this->geoplugin->locate($this->ip_address);
-		$data[0] = (array) $this->geoplugin;
+		$data[0]['ip']                 = $this->geoplugin->ip;
+		$data[0]['city']               = $this->geoplugin->city;
+		$data[0]['region']             = $this->geoplugin->region;
+		$data[0]['area_code']          = $this->geoplugin->areaCode;
+		$data[0]['dma_code']           = $this->geoplugin->dmaCode;
+		$data[0]['country_code']       = $this->geoplugin->countryCode;
+		$data[0]['country_name']       = $this->geoplugin->countryName;
+		$data[0]['continent_code']     = $this->geoplugin->continentCode;
+		$data[0]['latitude']           = $this->geoplugin->latitute;
+		$data[0]['longitude']          = $this->geoplugin->longitude;
+		$data[0]['currency_code']      = $this->geoplugin->currencyCode;
+		$data[0]['currency_symbol']    = $this->geoplugin->currencySymbol;
+		$data[0]['currency_converter'] = $this->geoplugin->currencyConverter;
+
 		return $this->EE->TMPL->parse_variables($this->EE->TMPL->tagdata, $data);
 	}
 }
