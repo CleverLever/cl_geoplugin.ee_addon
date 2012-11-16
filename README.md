623dc49f3b8e364bcd24adffe9026ae2d51b2ee7
Chris LeBlanc <chris@eedfw.us>
2012-11-16 13:34:45 -0600

    Adding single variable tags. Adding nearby and is_nearby functions

--- README.md
+++ README.md
@@ -12,16 +12,16 @@ Locates a user.
 
 #### Parameters
 
-+ ip_address
++ ip_address (default: user's ip_address)
 
-  Specify a users email address. Default: Current user's ip.
+  An ip address to geolocate.
 
 #### Variables
 
 ```
 {ip}
 {city}
-{region}
+{region} (two character state)
 {area_code}
 {dma_code}
 {country_name}
@@ -31,4 +31,41 @@ Locates a user.
 {currency_code}
 {currency_symbol}
 {currency_converter}
+{nearby}{/nearby}
 ```
+
+
+### {exp:eedfw_geoplugin:variable}
+
+Returns any of the variables available via the {exp:eedfw_geoplugin:locate} tag pair.
+
+
+### {exp:eedfw_geoplugin:is_nearby}
+
+Returns true or false whether the specified ip_address is in the nearby list that GeoPlugin provides.
+
+#### Parameters
+
++ ip_address (default: user's ip_address)
+
+  An ip address to geolocate.
+
++ city (required if lat/long not set)
+
+  A city you are trying to see if is nearby what was geolocated.
+
++ region (required if lat/long not set)
+
+  A region (two character state) you are trying to see if is nearby what was geolocated.
+
++ country_code (required if lat/long not set)
+
+  A country code (two character) you are trying to see if is nearby what was geolocated.
+
++ radius (default: 10)
+
+  Distance in miles from the geolocated ip_address to find nearby locations.
+  
++ limit (default: 100)
+
+  Used to limit the results since we are comparing a listing. In dense areas you might need to set it even higher.
--- mod.eedfw_geoplugin.php
+++ mod.eedfw_geoplugin.php
@@ -4,6 +4,9 @@ require_once('includes/geoplugin.class.php');
 class Eedfw_geoplugin
 {
 	private $settings;
+	private $ip_address;
+	private $geoplugin;
+	private $data = array();
 
 	public function __construct()
 	{
@@ -12,27 +15,89 @@ class Eedfw_geoplugin
 
 		$this->settings = $this->EE->Eedfw_geoplugin_settings_model->get();
 		$this->geoplugin = new geoPlugin();
+		
+		$this->ip_address = $this->EE->TMPL->fetch_param('ip_address', $this->EE->input->ip_address());
+
+		$this->geoplugin->locate($this->ip_address);
 	}
 
 	public function locate()
 	{
-		$this->ip_address = $this->EE->TMPL->fetch_param('ip_address', $this->EE->input->ip_address());
+		$this->data[0]['ip']                 = $this->geoplugin->ip;
+		$this->data[0]['city']               = $this->geoplugin->city;
+		$this->data[0]['region']             = $this->geoplugin->region;
+		$this->data[0]['area_code']          = $this->geoplugin->areaCode;
+		$this->data[0]['dma_code']           = $this->geoplugin->dmaCode;
+		$this->data[0]['country_code']       = $this->geoplugin->countryCode;
+		$this->data[0]['country_name']       = $this->geoplugin->countryName;
+		$this->data[0]['continent_code']     = $this->geoplugin->continentCode;
+		$this->data[0]['latitude']           = $this->geoplugin->latitude;
+		$this->data[0]['longitude']          = $this->geoplugin->longitude;
+		$this->data[0]['currency_code']      = $this->geoplugin->currencyCode;
+		$this->data[0]['currency_symbol']    = $this->geoplugin->currencySymbol;
+		$this->data[0]['currency_converter'] = $this->geoplugin->currencyConverter;
+		
+		$this->data[0]['nearby'] 			 = $this->_nearby($radius, $limit);
 
-		$this->geoplugin->locate($this->ip_address);
-		$data[0]['ip']                 = $this->geoplugin->ip;
-		$data[0]['city']               = $this->geoplugin->city;
-		$data[0]['region']             = $this->geoplugin->region;
-		$data[0]['area_code']          = $this->geoplugin->areaCode;
-		$data[0]['dma_code']           = $this->geoplugin->dmaCode;
-		$data[0]['country_code']       = $this->geoplugin->countryCode;
-		$data[0]['country_name']       = $this->geoplugin->countryName;
-		$data[0]['continent_code']     = $this->geoplugin->continentCode;
-		$data[0]['latitude']           = $this->geoplugin->latitude;
-		$data[0]['longitude']          = $this->geoplugin->longitude;
-		$data[0]['currency_code']      = $this->geoplugin->currencyCode;
-		$data[0]['currency_symbol']    = $this->geoplugin->currencySymbol;
-		$data[0]['currency_converter'] = $this->geoplugin->currencyConverter;
+		return $this->EE->TMPL->parse_variables($this->EE->TMPL->tagdata, $this->data);
+	}
+	
+	public function nearby($radius = 10, $limit = 10)
+	{
+		$radius			= $this->EE->TMPL->fetch_param('radius', $radius);
+		$limit			= $this->EE->TMPL->fetch_param('limit', $limit);
 
-		return $this->EE->TMPL->parse_variables($this->EE->TMPL->tagdata, $data);
+		$nearby = $this->_nearby($radius, $limit);
+
+		return $this->EE->TMPL->parse_variables($this->EE->TMPL->tagdata, $nearby);
+	}
+	
+	public function is_nearby($radius = 10, $limit = 100)
+	{
+		$city			= $this->EE->TMPL->fetch_param('city');
+		$region			= $this->EE->TMPL->fetch_param('region');
+		$country_code	= $this->EE->TMPL->fetch_param('country_code');
+		$radius			= $this->EE->TMPL->fetch_param('radius', $radius);
+		$limit			= $this->EE->TMPL->fetch_param('limit', $limit);
+		
+		$nearbys = $this->_nearby($radius, $limit);
+		
+		$data = FALSE;
+		foreach ($nearbys as $nearby) {
+			if ($nearby['city'] == $city && $nearby['region'] == $region && $nearby['country_code'] == $country_code) {
+				$data = TRUE;
+				break;
+			}
+		}
+
+		return $data;
+	}
+	
+	public function city() 					{ return $this->geoplugin->city; }
+	public function region() 				{ return $this->geoplugin->region; }	
+	public function area_code() 			{ return $this->geoplugin->area_code; }
+	public function dma_code() 				{ return $this->geoplugin->dma_code; }
+	public function country_code() 			{ return $this->geoplugin->country_code; }
+	public function continent_code() 		{ return $this->geoplugin->continent_code; }
+	public function latitude() 				{ return $this->geoplugin->latitude; }
+	public function longitude() 			{ return $this->geoplugin->longitude; }
+	public function currency_code() 		{ return $this->geoplugin->currency_code; }
+	public function currency_symbol() 		{ return $this->geoplugin->currency_symbol; }
+	public function currency_converter() 	{ return $this->geoplugin->currency_converter; }
+	
+	private function _nearby($radius = 10, $limit = 10)
+	{
+		$nearbys = $this->geoplugin->nearby($radius, $limit);
+
+		$row = 0;
+		foreach ($nearbys as $nearby) {
+			$this->data[$row]['city'] 			= $nearby['geoplugin_place'];
+			$this->data[$row]['region'] 			= $nearby['geoplugin_region'];
+			$this->data[$row]['country_code'] 	= $nearby['geoplugin_countryCode'];
+			$this->data[$row]['latitude']			= $nearby['geoplugin_latitude'];
+			$this->data[$row]['longitude']		= $nearby['geoplugin_longitude'];
+			$row++;
+		}
+	 	return $this->data;
 	}
 }
